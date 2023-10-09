19b8f698afb873b8f5b673016cf7e2dc7883e806
manuelageuzeveneficus <145553889+manuelageuzeveneficus@users.noreply.github.com>
2023-10-09 16:29:37 +0200

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -1,8 +1,9 @@
 import datetime
+import json
 import os
-import pickle as pkl
 
 import pandas as pd
+import requests
 import streamlit as st
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -39,72 +40,65 @@ except BaseException:
 
 # %% Layout Basic
 
-# Custom CSS to change the background color
-
-st.write(
-    """
-<style>
-body {
-    background-color: "#84e3f5";
-}
-</style>
-""",
-    unsafe_allow_html=True,
-)
-
 st.header(":blue[Deliverable koerier planning] :woman-biking:")
 st.write(
     "In deze interactieve webapp kunt u het aantal orders voor Deliverable voorspellen. U kunt een datum in de toekomst opgeven en de verwachte neerslag voor die dag. Aan de hand van die twee gegevens zal daaruit een voorspelling volgen voor het aantal orders voor die dag."
 )
 
-# %% Datum en Neerslag
-vandaag = datetime.date.today()
-datum = st.date_input(
-    "**Datum**", vandaag, min_value=vandaag, max_value=datetime.date(2023, 12, 31)
-)
-gem_neerslag = int(orders_maand[orders_maand.maand == datum.month].iloc[0, 1])
-neerslag = st.number_input(
-    "**De totale neerslag van regen, hagel en sneeuw (mm)** :rain_cloud:",
-    min_value=0,
-    step=1,
-    value=gem_neerslag,
-    help="Standaard is hier de gemiddelde neerslag van de maand ingevuld.",
-)
+col1, col2 = st.columns(2)
 
-if neerslag >= 50:
-    st.write("*Dat is erg veel neerslag* :cry:")
+# %% Datum en Neerslag
+with col1:
+    vandaag = datetime.date.today()
+    datum = st.date_input(
+        "**Datum :date:**", vandaag, min_value=vandaag, max_value=datetime.date(2023, 12, 31)
+    )
+
+with col2:
+    gem_neerslag = int(orders_maand[orders_maand.maand == datum.month].iloc[0, 1])
+    neerslag = st.number_input(
+        "**De totale neerslag (mm)** :rain_cloud:",
+        min_value=0,
+        step=1,
+        value=gem_neerslag,
+        help="Standaard is hier de gemiddelde neerslag van de maand ingevuld.",
+    )
+
+    if (neerslag >= 50) & (neerslag < 1000):
+        st.write("*Dat is erg veel neerslag* :cry:")
+
+    elif neerslag >= 1000:
+        st.write("*Zoek een*  :sailboat:")
 
 # %% Model inladen voor voorspelling
 
-df = pd.DataFrame(columns=["year", "month", "day"])
-df.loc[0] = [datum.year, datum.month, datum.day]
+# df = pd.DataFrame(columns=["year", "month", "day"])
+# df.loc[0] = [datum.year, datum.month, datum.day]
 
-# LOKAAL INLADEN
+# # LOKAAL INLADEN
 
-model_name = "lin_model.pkl"
-with open(
-    os.path.join(os.getenv(key="AZUREML_MODEL_DIR", default="./mlops/"), model_name), "rb"
-) as f:
-    model = pkl.load(f)
-prediction = model.predict(df)[0][0]
-
-
-# # INLADEN MODEL VAN API
-# scoring_uri = "http://34930671-f250-48e3-87e0-7cf2aa758b9b.westeurope.azurecontainer.io/score"
-# headers = {"Content-Type": "application/json"}
-# # resp = find out how to trigger your api with the requests package and valid data
-# body = "2023-10-6" {'date': datum.strftime("%Y-%m-%d"),'precipitation': neerslag}
+# model_name = "lin_model.pkl"
+# with open(
+#     os.path.join(os.getenv(key="AZUREML_MODEL_DIR", default="./mlops/"), model_name), "rb"
+# ) as f:
+#     model = pkl.load(f)
+# prediction = model.predict(df)[0][0]
 
 
-# resp = requests.post(scoring_uri, body, headers=headers)
+# INLADEN MODEL VAN API
+scoring_uri = "http://dbadb1ee-15c3-48be-968e-d083c304f9d7.westeurope.azurecontainer.io/score"
+headers = {"Content-Type": "application/json"}
+# resp = find out how to trigger your api with the requests package and valid data
+body = {"date": datum.strftime("%Y-%m-%d"), "precipitation": neerslag}
+body = json.dumps(body)
+
+resp = requests.post(scoring_uri, body, headers=headers)
 
 # st.text(resp.text)
 
 
 # Voorspelling
-st.markdown("**Voorspelde aantal orders:**")
-st.text(int(round(prediction, 0)))
-
-# %% GIF toevoegen
-
-st.markdown("![Alt Text](https://media.giphy.com/media/ICnE90q1JUH0poH7aB/giphy.gif)")
+st.write("---------------------------------------------------")
+st.markdown("**Voorspelling aantal orders :knife_fork_plate:**", unsafe_allow_html=True)
+# st.header(int(round(prediction, 0)))
+st.header(resp.text)
